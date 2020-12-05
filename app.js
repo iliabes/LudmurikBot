25ff9f8f869751c4e00d8706d5eba097cf5948c1
f0rGetFull <f0rgetful@yandex.ru>
2020-12-05 15:53:26 +0300

    almost done everthing

--- app.js
+++ app.js
@@ -3,38 +3,68 @@ const app = express();
 const port = process.env.PORT || 3000;
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1494999143:AAGekWMRiig2KcGN7pbIRyshMvUbVza88RI';
+const chatID = '-496670652'
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-  console.log(msg)
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+
+
+
+const joke = ['Колобок повесился','купил мужик шапку для танцев, а она ему как раз-два-три, раз-два-три...','"А вообще, главное не победа, а участие!" - задумчиво проговорил Гитлер, вынимая из сейфа браунинг.','При Путине возросли все показатели. Даже президентский срок вырос с 4 до 36 лет.','Большая пресс-конференция Владимира Путина пройдет 17 декабря. Ответы президента на вопросы можно уже сейчас посмотреть на сайте анекдотов.','В детский дом закупили сок «Моя семья».','Свитер, который колется, начал по ночам выносить из дома ценные вещи.','Купила феминистка обруч, а он ей как раз.'] 
+const reqvestBot = ['Сам бот','От бота слышу','Я не бот','Я человек'] 
+const requireGreeting = ['Привет мои дорогие','Приветсвие','Здрасте'] 
+const requireCool = ['Не искренне'] 
+const requireAnimal = ['joke1','joke2','joke3','joke4','joke5','joke6','joke7','joke8'] 
+const requireForecast = ['joke1','joke2','joke3','joke4','joke5','joke6','joke7','joke8'] 
+
+
+function randomInteger(min,max){
+  let rand = min - 0.5 + Math.random() * (max-min + 1);
+  return Math.round(rand)
+}
+
+bot.on('message', (msg) => {  
+  let mess = msg.text.split(' ')
+  mess.forEach((elem)=>{
+    console.log(elem)
+    if(elem == 'шутеечка' || elem == 'шутеечку' || elem == 'шутеечку'  || elem == 'шутка'  || elem == 'анекдот'  || elem == 'Шутеечку'){
+      bot.sendPhoto(chatID,'./img/1.jpeg',{caption: joke[randomInteger(0,joke.length-1)]})
+    }
+    if(elem == 'Привет' || elem == 'привет' || elem == 'Приветсвие'){
+      bot.sendMessage(chatID, requireGreeting[randomInteger(0,requireGreeting.length-1)]);
+    }
+    if(elem == 'Бот' || elem == 'бот'){
+      bot.sendMessage(chatID, reqvestBot[randomInteger(0,requireGreeting.length-1)]);
+    }
+    if(elem == 'Люда' || elem == 'Людмурик' || elem == 'Людка'){
+      bot.sendMessage(chatID, 'Да');
+    }
+    if(elem == 'молодец' ){
+      bot.sendMessage(chatID, 'Да ,он хороший человек');
+    }
+    if(elem == 'Круто' || elem == 'круто' || elem == 'крут' || elem == 'поздравляю' || elem == 'неплохо' || elem == 'Неплохо' || elem == 'Крут'){
+      bot.sendMessage(chatID, 'Неискренне!');
+    }
+  })
+  
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    console.log(msg)
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-
-});
 
 
-app.use('/',(req,res)=>{
-    res.send('this is /')
-})
+function intervalMess() {
+  bot.sendPhoto(chatID,`./img/${randomInteger(1,16)}.jpg`,{caption: joke[randomInteger(0,joke.length-1)]})
+}
+
+
+
+setInterval(intervalMess, 30500);
+
+
+
+//---------------------------------------
+
+
 
 app.listen(port,()=>{
     console.log('server is starting')
